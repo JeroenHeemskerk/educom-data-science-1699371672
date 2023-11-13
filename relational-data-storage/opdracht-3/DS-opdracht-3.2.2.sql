6574d1bf6bd0344a6f6e2e093dd67642284b7570
Nick Koole <nickkoole@hotmail.com>
2023-11-13 17:35:09 +0100

    #4 order by clausules weer weggehaald nu phpmyadmin geen error meer gaf

--- relational-data-storage/opdracht-3/DS-opdracht-3.2.2.sql
+++ relational-data-storage/opdracht-3/DS-opdracht-3.2.2.sql
@@ -1,5 +1,4 @@
 SELECT mhl_suppliers.name, mhl_suppliers.straat, mhl_suppliers.huisnr, mhl_suppliers.postcode
 FROM mhl_suppliers
 INNER JOIN mhl_membertypes ON mhl_suppliers.membertype = mhl_membertypes.id
-WHERE mhl_membertypes.name = 'Gold' OR 'Silver' OR 'Bronze' OR 'GEEN INTERESSE'
-ORDER BY mhl_suppliers.name;
+WHERE mhl_membertypes.name = 'Gold' OR 'Silver' OR 'Bronze' OR 'GEEN INTERESSE';
--- relational-data-storage/opdracht-3/DS-opdracht-3.2.3.sql
+++ relational-data-storage/opdracht-3/DS-opdracht-3.2.3.sql
@@ -1,5 +1,4 @@
 SELECT mhl_suppliers.name, mhl_suppliers.straat, mhl_suppliers.huisnr, mhl_suppliers.postcode
 FROM mhl_suppliers
 INNER JOIN mhl_cities ON mhl_suppliers.city_ID = mhl_cities.id
-WHERE mhl_cities.name = 'amsterdam' AND NOT mhl_suppliers.p_city_ID = 104
-ORDER BY mhl_suppliers.name;
+WHERE mhl_cities.name = 'amsterdam' AND NOT mhl_suppliers.p_city_ID = 104;
