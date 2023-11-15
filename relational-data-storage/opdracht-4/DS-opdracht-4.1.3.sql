6d89ec25f82bf1eec530768638dd566f1a270910
Nick Koole <nickkoole@hotmail.com>
2023-11-15 16:40:19 +0100

    #9

--- relational-data-storage/opdracht-4/DS-opdracht-4.1.3.sql
+++ relational-data-storage/opdracht-4/DS-opdracht-4.1.3.sql
@@ -1,8 +1,8 @@
-SELECT mhl_suppliers.name, mhl_suppliers.straat, mhl_suppliers.huisnr, mhl_suppliers.postcode
-FROM mhl_suppliers
-INNER JOIN mhl_cities ON mhl_suppliers.city_ID = mhl_cities.id AND mhl_cities.name = 'amsterdam'
-INNER JOIN mhl_suppliers_mhl_rubriek_view ON mhl_suppliers.id = mhl_suppliers_mhl_rubriek_view.mhl_suppliers_id
-INNER JOIN mhl_rubrieken AS A ON mhl_suppliers_mhl_rubriek_view.mhl_rubriek_view_ID = A.id
-LEFT JOIN mhl_rubrieken AS B ON B.parent = A.id
-WHERE A.name = 'drank' OR ((B.parent = A.id) AND A.name = 'drank')
-ORDER BY A.name, mhl_suppliers.name;
+SELECT A.name, S.name, S.straat, S.huisnr, S.postcode, A.name
+FROM mhl_suppliers AS S 
+INNER JOIN mhl_cities AS C ON S.city_ID = C.id
+INNER JOIN mhl_suppliers_mhl_rubriek_view AS SRV ON S.id = SRV.mhl_suppliers_id
+INNER JOIN mhl_rubrieken AS A ON SRV.mhl_rubriek_view_ID = A.id
+LEFT JOIN mhl_rubrieken AS B ON B.id = A.parent
+WHERE C.name = 'amsterdam' AND (A.name = 'drank' OR B.name = 'drank')
+ORDER BY A.name, S.name;
