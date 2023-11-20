9b4013a6d2e08dd8ea8dbd6bbd8370fd993bfa24
Nick Koole <nickkoole@hotmail.com>
2023-11-20 11:53:14 +0100

    #7 6.1.3 v2

--- relational-data-storage/opdracht-6/DS-opdracht-6.1.3.sql
+++ relational-data-storage/opdracht-6/DS-opdracht-6.1.3.sql
@@ -1,4 +1,4 @@
-SELECT RUBRIEKEN.name, SUPPLIER_COUNT.amount_of_suppliers
+SELECT RUBRIEKEN.name, IFNULL(SUPPLIER_COUNT.amount_of_suppliers, 0)
 FROM
 	(SELECT IF(child.parent = 0 OR parent.name IS NULL, child.name, CONCAT(parent.name, ' - ', child.name)) AS name,
     child.id as id
@@ -6,7 +6,7 @@ FROM
 	LEFT JOIN mhl_rubrieken AS parent ON parent.id = child.parent
 	ORDER BY name) AS RUBRIEKEN
 
-	INNER JOIN (SELECT SR.mhl_rubriek_view_ID AS rubriek_id, COUNT(SR.id) AS amount_of_suppliers
+	LEFT JOIN (SELECT SR.mhl_rubriek_view_ID AS rubriek_id, COUNT(SR.id) AS amount_of_suppliers
 	FROM mhl_suppliers_mhl_rubriek_view AS SR
 	GROUP BY rubriek_id
 	ORDER BY rubriek_id) AS SUPPLIER_COUNT ON RUBRIEKEN.id = SUPPLIER_COUNT.rubriek_id
