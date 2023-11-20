f52f69a28d3b52d4b5f32cdb043ba0552ef45ce0
Nick Koole <nickkoole@hotmail.com>
2023-11-20 14:43:05 +0100

    #7 6.1.2

--- relational-data-storage/opdracht-6/DS-opdracht-6.1.2.sql
+++ relational-data-storage/opdracht-6/DS-opdracht-6.1.2.sql
@@ -1,30 +1,29 @@
-SELECT
-TOTAL.gemeente,
-TOTAL.leverancier,
-TOTAL.totaal,
-AVERAGE.gemiddelde
-
+SELECT AVERAGE.gemeente, TOTAL.leverancier, TOTAL.totaal, AVERAGE.gemiddelde
 FROM
-	(SELECT
-	communes.id,
-    communes.name AS gemeente,
-	S.name AS leverancier,
-	SUM(H.hitcount) AS totaal
+	(SELECT	communes.id, S.name AS leverancier,	SUM(H.hitcount) AS totaal
 	FROM mhl_hitcount AS H
 	INNER JOIN mhl_suppliers AS S ON S.id = H.supplier_ID
     INNER JOIN mhl_cities AS cities ON cities.id = S.city_ID
     INNER JOIN mhl_communes AS communes ON communes.id = cities.commune_ID
+    INNER JOIN mhl_districts AS districts ON districts.id = communes.district_ID
+    WHERE districts.country_ID = 
+		(SELECT id
+		FROM mhl_countries
+        WHERE name = 'Nederland')
 	GROUP BY S.name) AS TOTAL
     
-    INNER JOIN
-    
-    (SELECT
-	communes.id,
-    communes.name AS gemeente,
-	S.name,
-	AVG(H.hitcount) AS gemiddelde
+    INNER JOIN (SELECT communes.id, communes.name AS gemeente, AVG(H.hitcount) AS gemiddelde
 	FROM mhl_hitcount AS H
 	INNER JOIN mhl_suppliers AS S ON S.id = H.supplier_ID
     INNER JOIN mhl_cities AS cities ON cities.id = S.city_ID
     INNER JOIN mhl_communes AS communes ON communes.id = cities.commune_ID
-	GROUP BY S.name) AS AVERAGE ON TOTAL.id = AVERAGE.id
+    INNER JOIN mhl_districts AS districts ON districts.id = communes.district_ID
+    WHERE districts.country_ID = 
+		(SELECT id
+		FROM mhl_countries
+        WHERE name = 'Nederland')
+    GROUP BY communes.id) AS AVERAGE ON TOTAL.id = AVERAGE.id
+
+GROUP BY AVERAGE.id, TOTAL.leverancier
+HAVING TOTAL.totaal > AVERAGE.gemiddelde
+ORDER BY AVERAGE.gemeente, (TOTAL.totaal - AVERAGE.gemiddelde) DESC
