72d56f79c653d580d1c7544db48d96101878c75a
Nick Koole <nickkoole@hotmail.com>
2023-11-21 09:29:56 +0100

    #7 6.3.2

--- relational-data-storage/opdracht-6/DS-opdracht-6.3.2.sql
+++ relational-data-storage/opdracht-6/DS-opdracht-6.3.2.sql
@@ -1,23 +1,18 @@
 SELECT
 S.name,
-CASE
-	WHEN S.name LIKE '%&euml;%'
-		THEN REPLACE(S.name, '&euml;', 'ë')
-    WHEN S.name LIKE '&Uuml;%'
-		THEN REPLACE(S.name, '&Uuml;', 'Ü')
-    WHEN S.name LIKE '%&ouml;%'
-		THEN REPLACE(S.name, '&ouml;', 'ö')
-    WHEN S.name LIKE '%&iuml;%'
-		THEN REPLACE(S.name, '&iuml;', 'ï')
-	WHEN S.name LIKE '%&auml;%'
-		THEN REPLACE(S.name, '&auml;', 'ä')
-	WHEN S.name LIKE '%&acirc;%'
-		THEN REPLACE(S.name, '&acirc;', 'ã')
-	WHEN S.name LIKE '%&eacute;%'
-		THEN REPLACE(S.name, '&eacute;', 'é')
-	WHEN S.name LIKE '%&egrave;%'
-		THEN REPLACE(S.name, '&egrave;', 'è')
-END as nicename
+REPLACE (
+	REPLACE (
+		REPLACE (
+			REPLACE (
+				REPLACE (
+					REPLACE (
+						REPLACE (name, '&euml;', 'ë'),
+                        '&ouml;', 'ö'),
+					'&Uuml;', 'Ü'),
+				'&eacute;', 'é'),
+			'&egrave;', 'è'),
+		'&iuml;', 'ï'),
+	'&acirc;', 'ã') AS nicename
 FROM mhl_suppliers AS S
 WHERE S.name LIKE '%&%;%'
 LIMIT 25;
